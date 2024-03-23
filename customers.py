9651005aca83f6649769922977983f543941b6f5
Lucca Wang <luccawang@gmail.com>
2024-03-23 16:42:47 -0600

    Adding notes to Melon class and helper funcs, defined Customer class - all for OOP, methods and attributes

--- customers.py
+++ customers.py
@@ -4,4 +4,27 @@
 class Customer:
     """Ubermelon customer."""
 
-    # TODO: need to implement this
+    #setting attributes by using self 
+    def __init__(
+            self,
+            first_name,
+            last_name,
+            email,
+            password,
+    ):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+    #handy for debugging, printing Customer instance object to terminal
+    def __repr__(self):
+        """Convenience method to show information about customer in console."""
+        
+        return (f"<Customer: {self.first_name, self.last_name}>")
+    
+#function to read data file, customers.txt
+def read_customers_from_file(filepath):
+    """Read customer information and populate dictionary of melon types."""
+
+    
--- melons.py
+++ melons.py
@@ -36,9 +36,7 @@ class Melon:
     def __repr__(self): #repr makes for an easy to understand version of a Melon object for reading in the terminal while developing
         """Convenience method to show information about melon in console."""
 
-        return (
-            f"<Melon: {self.melon_id}, {self.common_name}, {self.price_str()}>"
-        )
+        return (f"<Melon: {self.melon_id}, {self.common_name}, {self.price_str()}>")
     
 
 def read_melon_types_from_file(filepath):
@@ -63,7 +61,7 @@ def read_melon_types_from_file(filepath):
 
             price = float(price)
 
-            # For seedless, we want to turn "1" => True, otherwise False
+            #for seedless, we want to turn "1" => True, otherwise False
             seedless = seedless == "1"
 
             melon_types[melon_id] = Melon(
@@ -89,7 +87,7 @@ def get_all():
     [<Melon: 2, Crenshaw, $2.00>, <Melon: 14, Ali Baba Watermelon, $2.50>, <Melon: 15, Ancient Watermelon, $3.00>, <Melon: 16, Arkansas Black Watermelon, $4.00>, <Melon: 21, Chris Cross Watermelon, $2.50>, <Melon: 23, Congo Watermelon, $2.00>, <Melon: 25, Crimson Sweet Watermelon, $1.75>, <Melon: 27, Desert King Watermelon, $2.00>, <Melon: 28, Dixie Queen Watermelon, $2.00>, <Melon: 29, Moonbeam Watermon, $2.25>, <Melon: 30, Fairfax Watermelon, $2.00>, <Melon: 32, Golden Honey Watermelon, $2.50>, <Melon: 33, Golden Midget Watermelon, $2.50>, <Melon: 34, Hopi Yellow Watermelon, $2.50>, <Melon: 35, Irish Grey Watermelon, $2.50>, <Melon: 37, Jubilee Bush Watermelon, $2.50>, <Melon: 38, Jubilee Watermelon, $2.50>, <Melon: 42, Ledmon Watermelon, $3.00>, <Melon: 44, Malali Watermelon, $2.00>, <Melon: 45, Melitopolski Watermelon, $3.00>, <Melon: 48, Montenegro Man Melon, $2.50>, <Melon: 49, Moon and Stars Watermelon, $2.50>, <Melon: 52, Navajo Winter Watermelon, $3.00>, <Melon: 54, Orangeglo Watermelon, $2.75>, <Melon: 56, Royal Golden Watermelon, $2.25>, <Melon: 57, Scaly Bark Watermelon, $4.00>, <Melon: 58, Stone Mountain Watermelon, $3.00>, <Melon: 59, Sugar Baby Watermelon, $2.50>, <Melon: 60, Takii Gem Watermelon, $2.75>, <Melon: 61, Tendergold Watermelon, $2.50>, <Melon: 62, Texas Golden Watermelon, $2.75>, <Melon: 63, Thai Rom Dao Watermelon, $2.50>, <Melon: 64, Tom Watson Watermelon, $2.25>, <Melon: 66, White Wonder Watermelon, $2.50>, <Melon: 67, Wilson's Sweet Watermelon, $2.50>]
     """
 
-    # This relies on access to the global dictionary `melon_types`
+    #relies on access to the global dictionary `melon_types`
 
     return list(melon_types.values())
 
@@ -97,13 +95,12 @@ def get_all():
 def get_by_id(melon_id):
     """Return a melon, given its ID."""
 
-    # This relies on access to the global dictionary `melon_types`
+    #relies on access to the global dictionary `melon_types`
 
     return melon_types[melon_id]
 
 
-# Dictionary to hold types of melons.
-#
-# Format is {id: Melon object, ... }
+#dictionary to hold types of melons.
+#format is {id: Melon object, ... }
 
 melon_types = read_melon_types_from_file("melons.txt")
